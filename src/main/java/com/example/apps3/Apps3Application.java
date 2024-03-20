7bd95351b90b7dcecb473424a72b751ee350d1dc
formroller <54848570+formroller@users.noreply.github.com>
2024-03-20 16:00:14 +0900

    aws S3 bucket process

---
+++ src/main/java/com/example/apps3/Apps3Application.java
@@ -0,0 +1,13 @@
+package com.example.apps3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apps3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Apps3Application.class, args);
+    }
+
+}
